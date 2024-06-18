4db0392152a66bf71fd0363bd563ad653d1c8982
Ваня <7328652@gmail.com>
2024-06-18 16:14:36 +0300

    fix code in layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -3,23 +3,12 @@ import styles from "./default.module.scss";
 import { Navbar } from "@/components/navbar";
 import Footer from "@/components/Footer/Footer";
 
-export default function DefaultLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className={`${styles.layout} relative flex flex-col h-screen`}>
       <Navbar />
-<<<<<<< HEAD
-      <main className="container mx-auto max-w-7xl px-6 flex-grow mt-16">{children}</main>
+      <main className="container mx-auto max-w-[1440px] px-6 flex-grow mt-16 ">{children}</main>
       <Footer />
-=======
-      <main className="container mx-auto max-w-[1440px] px-6 flex-grow mt-16 ">
-        {children}
-      </main>
-      {/* <footer className="w-full flex items-center justify-center py-3"></footer> */}
->>>>>>> main
     </div>
   );
 }

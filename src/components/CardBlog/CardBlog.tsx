67fa2d1ad787c5a2ee85b5b3ae6aecdd31a4f951
Artchem <chemerys.artem@gmail.com>
2024-06-26 22:47:52 +0200

    translate CardDetail

--- src/components/CardBlog/CardBlog.tsx
+++ src/components/CardBlog/CardBlog.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Card, Image, Button } from "@nextui-org/react";
 
 import { CardBlogProps } from "@/types/CardBlogProps";
+import { useTranslation } from "react-i18next";
 
 const CardBlog: React.FC<CardBlogProps> = ({ data, onClickCard }) => {
+  const { t } = useTranslation();
+
   return (
     <>
       <li className=" " key={data.id}>
         <Card className="w-[330px] h-[430px] p-6  ">
           <div className="w-[280px] h-[250px] overflow-hidden group">
             <Image
-              alt={data.title}
+              alt={t(`${data.title}`)}
               className="w-full h-full object-contain transition-transform duration-300 group-hover:scale-110"
               src={data.img}
               // onClick={() => onClickCard()}
@@ -18,16 +21,21 @@ const CardBlog: React.FC<CardBlogProps> = ({ data, onClickCard }) => {
           </div>
 
           <h4
-            className="font-bold text-large mb-3 h-[56px] text-center "
+            className="font-bold text-large mb-3 h-[76px] text-center overflow-hidden"
             // onClick={() => onClickCard()}
           >
-            {data.title}
+            {t(`${data.title}`)}
           </h4>
           <p className="text-left h-[122px] mb-2 overflow-hidden text-ellipsis">
-            {data.description}
+            {t(`${data.description}`)}
           </p>
-          <Button onClick={onClickCard} className="ml-auto" size="sm" variant="light">
-            Read more
+          <Button
+            onClick={onClickCard}
+            className="ml-auto"
+            size="sm"
+            variant="light"
+          >
+            {t("blogBtn")}
           </Button>
         </Card>
       </li>
--- src/i18n/copies/en.ts
+++ src/i18n/copies/en.ts
@@ -137,4 +137,96 @@ export const en = {
   thankYouBtn1: "Read blog",
   thankYouBtn2: "Go to Home page",
   thankYouBtn3: "Сall us",
+
+  //blog
+  blogBtn: "Read more",
+  cardTitle1: "Good Faith within the framework of the Civil Code of Quebec",
+  descriptionCard1:
+    "Good faith occupies a central place in Quebec civil law, constituting a fundamental principle which influences and governs the interpretation and application of numerous legal provisions. Concretely, it manifests itself through two main dimensions: as a rule of conduct and as a criterion of interpretation.",
+  fullDescription_1_Title1: "Good faith as a rule of conduct",
+  fullDescription_1_Text1:
+    "In contractual relationships, good faith acts as both a behavioral requirement with respect to the parties during the formation, execution, and resolution of the contract.It requires parties to act with honesty, loyalty and in a manner that does not unduly harm the interests of the other party.This requirement of good faith is reflected in various aspects of the law of obligations, such as contractual negotiation, the exercise of contractual rights and the manner of fulfilling obligations.",
+  fullDescription_1_Title2: "Good faith as a criterion of interpretation",
+  fullDescription_1_Text2:
+    "Beyond its role in the conduct of the parties, good faith also serves as a guiding principle for the interpretation of contracts and legal obligations.It guides courts in investigating the intention of the parties and in the fair application of contractual provisions, ensuring that rights and obligations are exercised in a manner that respects the legitimate expectations of the parties.",
+  fullDescription_1_Title3:
+    "The application of good faith in specific situations",
+  fullDescription_1_Text3:
+    "In consumer contracts: Good faith is of particular importance, with legislators and courts seeking to protect consumers against unfair commercial practices and to ensure fairness in transactions.In matters of civil liability: Good faith influences the assessment of fault and can mitigate or aggravate the liability of the parties depending on their behavior before, during and after the occurrence of the damage.In the exercise of real rights: Good faith plays a role in the possession and acquisition of rights over property, where it can condition the acquisition of ownership or other real rights.",
+  fullDescription_1_Title4:
+    "Jurisprudence as a vector for the evolution of the notion of good faith",
+  fullDescription_1_Text4:
+    "Quebec jurisprudence has contributed to the evolution and precision of the notion of good faith, adapting it to the changing circumstances of society and the needs for justice between the parties.Court decisions illustrate the flexibility of good faith and its ability to adapt to a variety of legal situations, thereby reinforcing its status as an underlying principle in Quebec civil law.In conclusion, good faith constitutes a pillar of Quebec civil law, influencing both the conduct of the parties and the interpretation of legal norms.Its scope and application are broad, reflecting the importance of fairness, loyalty and trust in legal relationships.",
+
+  //card2
+  cardTitle2:
+    "Civil Liability: The Essence of Article 1457 of the Civil Code of Quebec",
+  descriptionCard2:
+    "In the complex fabric of our daily interactions, where every action can have an impact on others, article 1457 of the Civil Code of Quebec presents itself as a beacon of responsibility, illuminating the path towards justice and equity. This article, fundamental in the Quebec legal landscape, establishes a simple but profound principle: if our actions cause harm to someone, we have the obligation to repair the damage resulting from our faults. It is a constant reminder of the importance of our actions and their consequences in society",
+  fullDescription_2_Title1: "Fault: First Pillar of Civil Liability",
+  fullDescription_2_Text1:
+    "The notion of fault is central in the application of article 1457. It encompasses not only malicious intentional actions but also negligence, imprudence, and failures to fulfill our duties. Every time we deviate from the conduct expected of a reasonable person in similar circumstances, we cross the threshold of misconduct. This conceptual flexibility allows the law to adapt to the infinite nuances of real life, providing protection to those affected by the actions of others.",
+  fullDescription_2_Title2: "Prejudice: Second Keystone",
+  fullDescription_2_Text2:
+    "For there to be liability, there must be harm. This harm can be physical, moral, or material. It must be real, direct, and certain. Article 1457 does not simply punish the fault itself; he seeks to remedy the tangible consequences of this fault on the lives of others. It is the recognition that behind every dispute, there are impacted lives, perhaps shattered dreams, and a quest for reparation.",
+  fullDescription_2_Title3: "Reparation: Natural Conclusion of Responsibility",
+  fullDescription_2_Text3:
+    "The obligation to repair is the logical consequence of the fault and the damage. It can take various forms, ranging from financial compensation to restitution, or even reparation in kind. This flexibility allows the solution to be better adapted to the specific problem, seeking not only to compensate but also, wherever possible, to restore the previous state.",
+  fullDescription_2_Title4: "Towards a Responsible Future",
+  fullDescription_2_Text4:
+    "Article 1457, in its wisdom and simplicity, reminds us of the importance of vigilance and prudence in our actions. It embodies the idea that individual freedom finds its limit in respect for the rights of others. As a society, it invites us to pursue an ideal of respectful and responsible coexistence. Every day, as a lawyer, I see the impact this article has on people’s lives. It is not just a legal rule but a principle of life, a guide towards a fairer world. Whether you are the victim of harm or are seeking to understand your obligations, remember that behind legal terms there is a fundamental quest for justice and social harmony. Article 1457 of the Civil Code of Quebec is more than a legal provision; it is a pillar on which the balance of our society rests. By understanding and applying it, we all move forward together towards a future where responsibility and mutual respect are the cornerstones of our coexistence.",
+
+  //card3
+  cardTitle3: "THE PRESCRIPTION",
+  descriptionCard3:
+    "The limitation period for a personal law action is a complex and essential subject in the legal field, affecting the ability of individuals to take legal action to assert their rights. This article explores the fundamental principles of prescription in personal law, its practical implications, as well as its exceptions and particularities according to Quebec law.",
+  fullDescription_3_Title1: "What is Prescription in Personal Law?",
+  fullDescription_3_Text1:
+    "Prescription in personal law refers to a legal mechanism that limits the time available to bring legal action to claim the performance of an obligation or compensation for damage. The main objective of prescription is to guarantee legal certainty and the stability of social relations by preventing disputes from being reactivated indefinitely.",
+  fullDescription_3_Title2: "Limitation Periods",
+  fullDescription_3_Text2:
+    "In Quebec, the Civil Code of Quebec (CCQ) establishes the general rules of prescription. In principle, article 2925 of the CCQ provides that an action in personal law is prescribed by three years. This time limit means that, in the majority of cases, a creditor has three years from becoming aware of the right of action to initiate legal proceedings against a debtor. It is important to note that “knowledge of the right of action” is not limited to knowledge of the damage or obligation, but also includes knowledge of the person of the debtor. This clarification is intended to avoid situations where a creditor, unaware of the identity of the debtor, would be unfairly deprived of his right of action.",
+  fullDescription_3_Title3: "Exceptions and Particularities",
+  fullDescription_3_Text3:
+    "The CCQ provides for several exceptions to the general three-year deadline. For example, certain obligations arising from the operation of a business, actions for compensation for bodily injury caused by a criminal offense, or even certain claims based on private documents, may be subject to specific limitation periods. In addition, the law provides mechanisms that allow the prescription to be suspended or interrupted. Suspension of the prescription temporarily pauses the countdown of the period, without erasing the time already elapsed, while interrupting the prescription completely resets the countdown of the limitation period.",
+  fullDescription_3_Title4: "Conclusion",
+  fullDescription_3_Text4:
+    "The prescription of an action in personal law is a key concept in civil law, which balances the right of prosecution with the need to ensure a certain social peace. Limitation rules encourage parties to act diligently and resolve their disputes within a reasonable time frame, while providing flexibility through exceptions and suspension or interruption mechanisms. Understanding these rules is essential for anyone wishing to assert their rights in court.",
+
+  //card4
+  cardTitle4: "1855 MAÎTRES: Defending Your Interests with a Master’s Hand",
+  descriptionCard4:
+    "Protect your rights, adapt legal solutions with expertise and care. Need a lawyer? The legal world can often seem complex and intimidating, but with 1-855-MAÎTRES at your side, you can rest assured that your interests will be expertly defended. Founded in 2021 by Maître Sophie Mongeon, 1-855-MAÎTRES is much more than a simple legal service – it is a partner in defending your rights and resolving your legal problems.",
+  fullDescription_4_Title1: "WTailor-made legal solutions",
+  fullDescription_4_Text1:
+    "Our team of highly qualified lawyers offers tailor-made legal solutions in various areas to meet your specific needs. Whether you are facing civil law, insurance law, criminal and penal law, or labor and social law issues, we have the expertise to guide you through every step of the process.                           1.Civil Law: Our civil law attorneys are prepared to develop legal solutions tailored to your needs, providing you with effective and ethical representation in all civil matters. 2.Insurance Law: If your insurance companies refuse to pay you, we are here to assert your rights. Our team will help you obtain the compensation to which you are entitled. 3.Criminal and Penal Law: When faced with justice, you are not alone. Benefit from the recognized expertise of our lawyers in criminal and penal law to ensure a solid defense. 4.Labor and Social Law: Whatever your needs in the labor and social field, our tailor-made legal solutions will guide and support you.",
+  fullDescription_4_Title2: "The Democratization of Law",
+  fullDescription_4_Text2:
+    "1-855-MAÎTRES represents the democratization of law by guaranteeing rapid and affordable access to competent and knowledgeable experts. Our lawyers respect strict criteria of efficiency, ethics, commitment, fairness and transparency, thus ensuring quality legal representation.",
+  fullDescription_4_Title3: "Your Legal Partner in Quebec",
+  fullDescription_4_Text3:
+    "1-855-MAÎTRES is the legal service in Quebec dedicated to providing comprehensive and accessible legal services. Regardless of the nature of your legal situation, we work tirelessly to defend your rights and interests. Our consultations are affordable because we believe in providing legal advice that is accessible to everyone. If you are looking for a solution to your legal problems, contact us without delay at 1-855-MAÎTRES . Our office is located at 4350 Rue Beaubien E, Montreal, QC H1T 1S9 . Take the first step toward resolving your legal issues with the expert assistance of 1-855-MAÎTRES.",
+
+  //card3
+  cardTitle5:
+    "Civil Law Demystified with 1-855 MAÎTRES: Your Legal Guide in Quebec",
+  descriptionCard5:
+    "Civil Law, a vast and complex legal field, closely affects many aspects of our daily lives. Whether it involves contracts, civil liability, litigation or inheritance, a thorough understanding of Civil Law is crucial to successfully navigating these situations. At 1-855 MAÎTRES, our team of specialized lawyers is here to simplify Civil Law and offer you legal solutions adapted to your specific needs.",
+  fullDescription_5_Title1: "What is Civil Law?",
+  fullDescription_5_Text1:
+    "Civil Law encompasses a set of rules and principles that govern relationships between individuals and ensure justice and fairness in these interactions. It covers a wide range of areas, including contracts, civil liability, litigation, and inheritance.",
+  fullDescription_5_Title2: "Legal Services in Civil Law with 1-855 MASTERS",
+  fullDescription_5_Text2:
+    "Our team of experienced lawyers at 1-855 MAÎTRES offers a full range of legal services in Civil Law to support you in various aspects of your legal life, including: 1 Contracts and Obligations: Drafting, interpretation and disputes related to contracts. 2 Civil Liability: Defense of rights in the event of damage suffered. 3 Civil Disputes: Resolution of disputes before civil courts. 4 Inheritance: Advice and assistance in inheritance matters.",
+  fullDescription_5_Title3:
+    "Why Choose 1-855 MAÎTRES for Your Civil Law Matters?",
+  fullDescription_5_Text3:
+    "Specialized Expertise: Our lawyers specializing in Civil Law have in-depth knowledge of the laws and regulations that govern this complex area. 2 Personalized Approach: Each case is unique. We take a personalized approach to understand your specific needs and develop tailor-made solutions. 3 Transparency and Communication: We believe in transparent communication with our customers. You will be informed of every step of the legal process. 4Financial Accessibility: Justice must be accessible to all. We offer affordable consultations to help you get the legal assistance you need.",
+  fullDescription_5_Title4: "Contact 1-855 MAÎTRES for Your Civil Law Needs",
+  fullDescription_5_Text4:
+    "If you are faced with legal questions in Civil Law, look no further. Contact us at 1-855-MAÎTRES (1855-624-8737) or by e-mail at Info@1855maitres-com. At 1-855 MAÎTRES, we are here to assist you at every step of your legal journey by offering you solutions tailored to your specific needs. Your interests will be defended expertly! Stay informed and protected in the complex world of Civil Law with 1-855 MASTERS!",
+
+  //blogDetail
+
+  cardDetailBtn: "See all",
 };

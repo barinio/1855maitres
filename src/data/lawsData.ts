e8a8e904f4f6dfe8cc62e1caf57fe09e5d54c8cc
Avis <youareaviss@gmail.com>
2024-07-02 16:23:50 -0400

    index.js

--- src/data/lawsData.ts
+++ src/data/lawsData.ts
@@ -1,40 +1,40 @@
-import civilLawImg from "@/images/civil-law.png";
-import criminalAndPenalLawwImg from "@/images/criminal-and-penal-law.png";
-import immigrationLawImg from "@/images/immigration-to-canada.png";
-import insuranceLawImg from "@/images/insurance-law.png";
-import laborAndSocialLawImg from "@/images/labor-and-social-law.png";
-import otherTypeOfLawImg from "@/images/area-of-law.png";
-import { LawItem } from "@/types/ListItemProps";
+import civilLawImg from '@/images/civil-law.png';
+import criminalAndPenalLawwImg from '@/images/Criminal.png';
+import immigrationLawImg from '@/images/Immigration.png';
+import insuranceLawImg from '@/images/Insurance.png';
+import laborAndSocialLawImg from '@/images/social.jpg';
+import otherTypeOfLawImg from '@/images/area-of-law.png';
+import { LawItem } from '@/types/ListItemProps';
 
 export const LawsData: LawItem[] = [
   {
-    title: "lawsTitle1",
-    description: "lawsDescription",
-    img: civilLawImg
+    title: 'lawsTitle1',
+    description: 'lawsDescription',
+    img: civilLawImg,
   },
   {
-    title: "lawsTitle2",
-    description: "lawsDescription",
-    img: criminalAndPenalLawwImg
+    title: 'lawsTitle2',
+    description: 'lawsDescription',
+    img: criminalAndPenalLawwImg,
   },
   {
-    title: "lawsTitle3",
-    description: "lawsDescription",
-    img: immigrationLawImg
+    title: 'lawsTitle3',
+    description: 'lawsDescription',
+    img: immigrationLawImg,
   },
   {
-    title: "lawsTitle4",
-    description: "lawsDescription",
-    img: insuranceLawImg
+    title: 'lawsTitle4',
+    description: 'lawsDescription',
+    img: insuranceLawImg,
   },
   {
-    title: "lawsTitle5",
-    description: "lawsDescription",
-    img: laborAndSocialLawImg
+    title: 'lawsTitle5',
+    description: 'lawsDescription',
+    img: laborAndSocialLawImg,
   },
   {
-    title: "lawsTitle6",
-    description: "lawsDescription",
-    img: otherTypeOfLawImg
-  }
+    title: 'lawsTitle6',
+    description: 'lawsDescription',
+    img: otherTypeOfLawImg,
+  },
 ];

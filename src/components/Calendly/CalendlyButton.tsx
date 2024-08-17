a7bed200f0cfe354b317420e4d814268edf942dc
Avis <youareaviss@gmail.com>
2024-08-17 10:08:54 -0400

    index.js

--- src/components/Calendly/CalendlyButton.tsx
+++ src/components/Calendly/CalendlyButton.tsx
@@ -8,7 +8,14 @@ import axios from "axios";
 interface CalendlyButtonProps {
   buttonText: string;
   color: "primary" | "default" | "secondary" | "success" | "warning" | "danger";
-  variant: "solid" | "bordered" | "light" | "flat" | "faded" | "shadow" | "ghost";
+  variant:
+    | "solid"
+    | "bordered"
+    | "light"
+    | "flat"
+    | "faded"
+    | "shadow"
+    | "ghost";
   className: string;
   style: {};
 }
@@ -20,7 +27,13 @@ interface CalendlyEventData {
   startTime: string;
 }
 
-function CalendlyButton({ buttonText, color, variant, className, style }: CalendlyButtonProps) {
+function CalendlyButton({
+  buttonText,
+  color,
+  variant,
+  className,
+  style,
+}: CalendlyButtonProps) {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const { t } = useTranslation();
@@ -32,7 +45,10 @@ function CalendlyButton({ buttonText, color, variant, className, style }: Calend
     console.log("eventData:", eventData);
 
     try {
-      const response = await axios.post("http://localhost:3000/api/calendly-events", eventData);
+      const response = await axios.post(
+        "https://one855-product-code-1.onrender.com/api/calendly-events",
+        eventData
+      );
 
       // eslint-disable-next-line no-console
       console.log("Data sent successfully:", response.data);
@@ -43,19 +59,19 @@ function CalendlyButton({ buttonText, color, variant, className, style }: Calend
   };
 
   useCalendlyEventListener({
-    onEventScheduled: (e) => {
+    onEventScheduled: e => {
       // Asserting the type of e.data.payload to any to bypass strict type checking
       const eventData: CalendlyEventData = {
         name: (e.data.payload as any).invitee?.name,
         profileName: (e.data.payload as any).event?.name,
         phoneNumber: (e.data.payload as any).invitee?.phone_number || "",
-        startTime: (e.data.payload as any).event?.start_time
+        startTime: (e.data.payload as any).event?.start_time,
       };
 
       sendDataToBackend(eventData);
       setIsOpen(false);
       navigate("/thank-you");
-    }
+    },
   });
 
   return (
@@ -71,7 +87,7 @@ function CalendlyButton({ buttonText, color, variant, className, style }: Calend
       </Button>
 
       <PopupModal
-        url="https://calendly.com/7328652/test"
+        url="https://calendly.com/1855maitres/reunion-de-15-minutes-en-teams"
         onModalClose={() => setIsOpen(false)}
         open={isOpen}
         rootElement={refRootElement}
